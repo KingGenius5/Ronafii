7d7d521319bd3066a88c379b2c072658500d5f17
Muhammad Tifak <moht20009@gmail.com>
2020-10-19 11:57:49 -0400

    Checkout + React Context API

--- ronafii/src/App.js
+++ ronafii/src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header'
-import Home from './Home'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     // BEM styling convention
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>CHCKOUT PAGE</h1>
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
